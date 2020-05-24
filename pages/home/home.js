80debb0b7c7b475fd74ae1dfbdb8656594afe837
fengxiaofang1 <fengxiaofang11@163.com>
2020-05-24 17:17:35 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,66 @@
 // pages/home/home.js
+//getApp()获取app()产生的实例对象
+const app = getApp()
+console.log(app.globalData.name)
+console.log(app.globalData.age)
+
+//注册一个页面
 Page({
 
   /**
    * 页面的初始数据
    */
+  //--------2.初始化一些数据---------
   data: {
-
+    name:'coderwhy',
+    age:18,
+    message: '哈哈哈',
+    list:[]
+  },
+  
+  //--------- 1.监听页面的生命周期函数--------
+  onLoad(){
+    console.log('监听页面加载:onLoad');
+    wx.request({
+      url: 'url',
+      success: (res)=>{
+        console.log(res)
+        const data = res.data.data.list;
+        this.setData({
+          list:data
+        })
+      }
+    })
+  },
+  onShow(){
+    console.log('监听页面显示:onShow');
+  },
+  onReady(){
+    console.log('监听页面初次渲染完成:onReady');
+  }, 
+  onHide(){
+    console.log('监听页面隐藏:onHide');
+  },
+  onUnload(){
+    console.log('监听页面卸载:onUnload');
+  },
+  //---------3.监听wxml中相关的一些事情--------
+  hangleGetUserInfo(event){
+    console.log(event)
+  },
+  handleViewClick(){
+    console.log('哈哈哈被点击了')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //----------4.其他事件------------
+  //监听页面滚动
+  onPageScroll(obj){
+    console.log(obj)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onReachBottom(){
+    console.log('页面滚动到底部')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh(){
+    console.log('下拉刷新')
   }
 })
