6f1dc44d69d3d28c6d42bcf21440344ed9fd874b
fengxiaofang1 <fengxiaofang11@163.com>
2020-05-30 21:50:36 +0800

    0505_展示弹框和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,64 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中...',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: function(){
+        console.log('展示弹窗成功')
+      },
+      fail: function(){
+        console.log('展示弹窗失败')
+      },
+      complete: function(){
+        console.log('完成函数的调用')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModel(){
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      //cancelText: '退出',
+      cancelColor: 'red',
+      success: function(res){
+        if(res.confirm){
+          console.log('用户点击了确定')
+        }else{
+          console.log('用户点击了取消')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    }),
+    setTimeout(() => {
+      //必须手动hideLoading才会让loading消失  
+      wx.hideLoading() 
+    },5000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleshowActionSheet(){
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'green',
+      success: function(res){
+        console.log(res.tapIndex)
+      },
+      fail: function(res){
+        console.log(res.errMsg)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options){
+    return{
+      title: '你好啊！',
+      path: '/pages/about/about.wxml',
+      imageUrl: "../../asserts/tupian.jpg"
+    }
   }
 })
