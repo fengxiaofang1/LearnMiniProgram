f894404898c93b46f5302a5963ed57a0c4a82844
fengxiaofang1 <fengxiaofang11@163.com>
2020-05-31 01:12:10 +0800

    07_页面跳转

---
+++ pages/detail/detail.js
@@ -0,0 +1,29 @@
+// pages/detail/detail.js
+Page({
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options)
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    //1.获取首页的页面对象
+    //getCurrentPages当前所有栈的页面
+    const pages = getCurrentPages()
+    const home = pages[pages.length - 2]
+
+    //2.调用页面对象的setData
+    home.setData({
+      title: '呵呵呵'
+    })
+  },
+  handleBackHome(){
+    wx.navigateBack({
+      delta: 1
+    })
+  }
+})
